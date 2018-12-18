81976e3dde1f0a056380f95f188b9208c3ddd5fe
Saurabh Raje <saurabh.mraje@gmail.com>
2018-12-18 13:35:01 +0100

    updated logged experiments

--- src/main.rs
+++ src/main.rs
@@ -6,13 +6,14 @@ mod sequential_algorithm;
 mod wrapper_functions;
 use self::wrapper_functions::*;
 use grouille::Point;
+#[macro_use]
+extern crate itertools;
 use rand::random;
 #[cfg(feature = "rayonlogs")]
 use rayon_logs::ThreadPoolBuilder;
 use std::iter::repeat_with;
 const THRESHOLD_DISTANCE: f64 = 0.01;
 const NUM_POINTS: usize = 100_000;
-const NUM_THREADS: usize = 4;
 const RUNS_NUMBER: usize = 5;
 
 fn get_random_points(num_points: usize) -> Vec<Point> {
@@ -24,23 +25,36 @@ fn get_random_points(num_points: usize) -> Vec<Point> {
 fn main() {
     #[cfg(feature = "rayon_logs")]
     {
-        let pool = rayon_logs::ThreadPoolBuilder::new()
-            .num_threads(NUM_THREADS)
-            .build()
-            .expect("logging pool creation failed");
-        let input = get_random_points(NUM_POINTS);
-        //pool.install(|| wrapper_parallel(&input, THRESHOLD_DISTANCE))
-        //    .1
-        //    .save_svg("parallel_rayon.html")
-        //    .expect("Failed");
-        //pool.install(|| wrapper_parallel_opt(&input, THRESHOLD_DISTANCE))
-        //    .1
-        //    .save_svg("parallel_rayon_opt.html")
-        //    .expect("Failed");
-        pool.install(|| wrapper_parallel_adaptive(&input, THRESHOLD_DISTANCE))
-            .1
-            .save_svg("parallel_adaptive.html")
-            .expect("Failed");
+        let thread_nums = vec![3, 7, 11, 13, 16];
+        let numbers_of_points = vec![50_000, 200_000, 300_000];
+        let thresholds = vec![0.01, 0.1, 0.5];
+        //let multi_prod = vec![thread_nums, numbers_of_points, thresholds]
+        //    .into_iter()
+        //    .multi_cartesian_product();
+        //        thread_nums.into_iter().for_each(|num_threads| {
+        //            numbers_of_points.into_iter().for_each(|num_points| {
+        //                thresholds.into_iter().for_each(|threshold_distance| {
+        for (num_threads, num_points, threshold_distance) in iproduct!(
+            thread_nums.into_iter(),
+            numbers_of_points.into_iter(),
+            thresholds.into_iter()
+        ) {
+            let pool = rayon_logs::ThreadPoolBuilder::new()
+                .num_threads(num_threads)
+                .build()
+                .expect("logging pool creation failed");
+            let input = get_random_points(num_points);
+            pool.install(|| wrapper_parallel_adaptive(&input, threshold_distance))
+                .1
+                .save_svg(format!(
+                    "parallel_adaptive_{}_threads_{}_pts_{}_thresh.html",
+                    num_threads, num_points, threshold_distance
+                ))
+                .expect("Failed");
+        }
+        //                });
+        //            });
+        //        });
     }
     #[cfg(not(feature = "rayon_logs"))]
     {
