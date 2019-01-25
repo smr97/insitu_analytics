8055760d0f963bd04ba373333c4ce5a51424698b
Saurabh Raje <saurabh.mraje@gmail.com>
2019-01-25 15:30:13 +0100

    experiment driver code

--- src/main.rs
+++ src/main.rs
@@ -5,16 +5,17 @@ mod parallel_rayon;
 mod sequential_algorithm;
 mod wrapper_functions;
 use self::wrapper_functions::*;
+#[cfg(feature = "rayon_logs")]
+use crate::sequential_algorithm::*;
 use grouille::Point;
 #[macro_use]
 extern crate itertools;
-#[cfg(feature = "rayon_logs")]
-use crate::sequential_algorithm::*;
+use itertools::Itertools;
 use rand::random;
 #[cfg(feature = "rayon_logs")]
 use rayon_adaptive::prelude::*;
 #[cfg(feature = "rayon_logs")]
-use rayon_logs::ThreadPoolBuilder;
+use rayon_logs::{RunLog, Stats, ThreadPoolBuilder};
 use std::iter::repeat_with;
 const THRESHOLD_DISTANCE: f64 = 0.01;
 const NUM_POINTS: usize = 100_000;
@@ -26,17 +27,53 @@ fn get_random_points(num_points: usize) -> Vec<Point> {
         .collect()
 }
 
+#[cfg(feature = "rayon_logs")]
+fn print_stats(run_log: RunLog, num_threads: usize) {
+    let vec_run_logs = vec![vec![run_log]];
+    let stats = Stats::get_statistics(&vec_run_logs, num_threads as f64, 1.0);
+    println!("The total times are");
+    for time in stats.total_times() {
+        println!("{}, ", time);
+    }
+    println!("The idle times are");
+    for time in stats.idle_times() {
+        println!("{}, ", time);
+    }
+    println!("The tagged times are");
+    for time in stats.sequential_times() {
+        println!("{:?}, ", time);
+    }
+    println!(
+        "Number of tasks is {}\n",
+        vec_run_logs[0][0].tasks_logs.len()
+    );
+    //vec_run_logs[0][0]
+    //    .save(format!(
+    //        "parallel_adaptive_{}_threads_{}_pts_{}_thresh.json",
+    //        num_threads, num_points, threshold_distance
+    //    ))
+    //    .expect("Failed");
+}
+
 fn main() {
     #[cfg(feature = "rayon_logs")]
     {
-        let thread_nums = vec![3, 7, 11, 13, 16];
-        let numbers_of_points = vec![50_000, 200_000, 300_000];
-        let thresholds = vec![0.01, 0.1, 0.5];
+        let thread_nums = vec![2];
+        let numbers_of_points = vec![200_000];
+        let thresholds = vec![0.01, 0.05, 0.1, 0.3, 0.5];
         for (num_threads, num_points, threshold_distance) in iproduct!(
             thread_nums.into_iter(),
             numbers_of_points.into_iter(),
             thresholds.into_iter()
         ) {
+            println!(
+                "number of threads {}, number of points {}, threshold {}",
+                num_threads, num_points, threshold_distance
+            );
+            let pool = rayon_logs::ThreadPoolBuilder::new()
+                .num_threads(num_threads)
+                .build()
+                .expect("logging pool creation failed");
             let input = get_random_points(num_points);
             let squares = hash_points(&input, threshold_distance);
             let hashing_offsets = vec![
@@ -45,30 +82,55 @@ fn main() {
                 (0.0, threshold_distance),
                 (threshold_distance, threshold_distance),
             ];
-            let pool = rayon_logs::ThreadPoolBuilder::new()
-                .num_threads(num_threads)
-                .build()
-                .expect("logging pool creation failed");
-            pool.install(|| {
-                squares
-                    .into_adapt_iter()
-                    .zip(hashing_offsets.into_adapt_iter())
-                    .map(|(square, hashing_offset)| {
-                        Graph::adaptive_parallel_new(
-                            &square,
-                            &input,
-                            threshold_distance,
-                            *hashing_offset,
-                        )
-                    })
-                    .collect::<Vec<_>>()
-            })
-            .1
-            .save_svg(format!(
-                "parallel_adaptive_{}_threads_{}_pts_{}_thresh.html",
-                num_threads, num_points, threshold_distance
-            ))
-            .expect("Failed");
+            let run_log = pool
+                .install(|| {
+                    squares
+                        .into_adapt_iter()
+                        .zip(hashing_offsets.into_adapt_iter())
+                        .map(|(square, hashing_offset)| {
+                            Graph::adaptive_parallel_new(
+                                &square,
+                                &input,
+                                threshold_distance,
+                                *hashing_offset,
+                            )
+                        })
+                        .collect::<Vec<_>>()
+                })
+                .1;
+            println!("Adaptive stats:");
+            print_stats(run_log, num_threads);
+            let run_log = pool
+                .install(|| {
+                    squares
+                        .into_adapt_iter()
+                        .zip(hashing_offsets.into_adapt_iter())
+                        .map(|(square, hashing_offset)| {
+                            Graph::parallel_new_opt(
+                                &square,
+                                &input,
+                                threshold_distance,
+                                *hashing_offset,
+                            )
+                        })
+                        .collect::<Vec<_>>()
+                })
+                .1;
+            println!("Rayon opt stats:");
+            print_stats(run_log, num_threads);
+            let run_log = pool
+                .install(|| {
+                    squares
+                        .into_adapt_iter()
+                        .zip(hashing_offsets.into_adapt_iter())
+                        .map(|(square, hashing_offset)| {
+                            Graph::new(&square, &input, threshold_distance, *hashing_offset)
+                        })
+                        .collect::<Vec<_>>()
+                })
+                .1;
+            println!("Sequential stats:");
+            print_stats(run_log, num_threads);
         }
     }
     #[cfg(not(feature = "rayon_logs"))]
@@ -82,44 +144,39 @@ fn main() {
                 1, sequential_time_seconds, number_of_points
             );
         });
-        (2..17).for_each(|thread_num| {
-            if thread_num % 2 == 1 {
-                ()
-            } else {
-                let pool =
-                    thread_binder::BindableThreadPool::new(thread_binder::POLICY::ROUND_ROBIN_CORE)
-                        .num_threads(thread_num)
-                        .build()
-                        .expect("Pool creation failed");
-                pool.install(|| {
-                    (0..RUNS_NUMBER).for_each(|run| {
-                        let number_of_points = NUM_POINTS + run * 100_000;
-                        let input = get_random_points(number_of_points);
-                        let parallel_rayon_time_seconds =
-                            wrapper_parallel(&input, THRESHOLD_DISTANCE);
-                        println!(
-                            "RAYON PARALLEL, {}, {}, {}",
-                            thread_num, parallel_rayon_time_seconds, number_of_points
-                        );
+        (13..14).for_each(|thread_num| {
+            let pool =
+                thread_binder::BindableThreadPool::new(thread_binder::POLICY::ROUND_ROBIN_CORE)
+                    .num_threads(thread_num)
+                    .build()
+                    .expect("Pool creation failed");
+            pool.install(|| {
+                (0..RUNS_NUMBER).for_each(|run| {
+                    let number_of_points = NUM_POINTS + run * 100_000;
+                    let input = get_random_points(number_of_points);
+                    let parallel_rayon_time_seconds = wrapper_parallel(&input, THRESHOLD_DISTANCE);
+                    println!(
+                        "RAYON PARALLEL, {}, {}, {}",
+                        thread_num, parallel_rayon_time_seconds, number_of_points
+                    );
 
-                        let input = get_random_points(number_of_points);
-                        let parallel_rayon_opt_time_seconds =
-                            wrapper_parallel_opt(&input, THRESHOLD_DISTANCE);
-                        println!(
-                            "RAYON PARALLEL OPT, {}, {}, {}",
-                            thread_num, parallel_rayon_opt_time_seconds, number_of_points
-                        );
+                    let input = get_random_points(number_of_points);
+                    let parallel_rayon_opt_time_seconds =
+                        wrapper_parallel_opt(&input, THRESHOLD_DISTANCE);
+                    println!(
+                        "RAYON PARALLEL OPT, {}, {}, {}",
+                        thread_num, parallel_rayon_opt_time_seconds, number_of_points
+                    );
 
-                        let input = get_random_points(number_of_points);
-                        let parallel_adaptive_time_seconds =
-                            wrapper_parallel_adaptive(&input, THRESHOLD_DISTANCE);
-                        println!(
-                            "ADAPTIVE PARALLEL OPT, {}, {}, {}",
-                            thread_num, parallel_adaptive_time_seconds, number_of_points
-                        );
-                    })
-                });
-            }
+                    let input = get_random_points(number_of_points);
+                    let parallel_adaptive_time_seconds =
+                        wrapper_parallel_adaptive(&input, THRESHOLD_DISTANCE);
+                    println!(
+                        "ADAPTIVE PARALLEL OPT, {}, {}, {}",
+                        thread_num, parallel_adaptive_time_seconds, number_of_points
+                    );
+                })
+            });
         });
     }
 }
